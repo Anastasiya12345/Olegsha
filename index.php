44db84c358393a82854a13207fa9889b94a3abfe
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-21 10:27:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,36 @@
 <head>
 <Title>Registration Form</Title>
 <style type="text/css">
-    body { background-color:
- #fff; border-top: solid 10px #000;
- color: #333; font-size: .85em;
- margin: 20; padding: 20;
- font-family: "Segoe UI",
- Verdana, Helvetica, Sans-Serif;
+    body { 
+        background-color: #fff; 
+        border-top: solid 10px #000;
+        color: #333; font-size: .85em;
+        margin: 20; padding: 20;
+        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
     }
-    h1, h2, h3,{ color: #000; 
-margin-bottom: 0; padding-bottom: 0; }
+    
+    h1, h2, h3,{ 
+        color: #000; 
+        margin-bottom: 0; 
+        padding-bottom: 0; 
+    }
+    
     h1 { font-size: 2em; }
     h2 { font-size: 1.75em; }
     h3 { font-size: 1.2em; }
     table { margin-top: 0.75em; }
-    th { font-size: 1.2em;
- text-align: left; border: none; padding-left: 0; }
-    td { padding: 0.25em 2em 0.25em 0em; 
-border: 0 none; }
+    
+    th { 
+        font-size: 1.2em;
+        text-align: left; 
+        border: none; 
+        padding-left: 0; 
+    }
+    
+    td { 
+        padding: 0.25em 2em 0.25em 0em; 
+        border: 0 none; 
+    }
 </style>
 </head>
 <body>
@@ -33,6 +46,7 @@ enctype="multipart/form-data" >
       <select><option>Выберите пол</option>
         <option value="S1">Муж</option>
         <option value="S2">Жен</option>
+      </select>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
