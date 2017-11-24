39516701f6fa9bd607d54d6c1e78fa14ceaeaa47
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-24 17:35:45 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -2,7 +2,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tbl1(
+    $sql = "CREATE TABLE registration_tb(
     id INT NOT NULL IDENTITY(1,1), 
     PRIMARY KEY(id),
     name VARCHAR(30),
