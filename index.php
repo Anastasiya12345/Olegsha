5274e10b8c2c71682351563baffa71a1e68097db
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-24 17:54:05 +0400

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
         <input type="text" name="name" id="name" placeholder="Введите ваше имя">
         <input type="text" name="email" id="email" placeholder="Ваш еmail..">
         <input type="submit" name="submit" class="btn" value="Отправить">
-        <div class="cl">
-          <input type="submit" name="clear" class="btn" value="Очистить">
-        </div>
+        
+          <input type="submit" name="clear" class="cl" value="Очистить">
+        
       </div>
       <div>
         <select name="gender" id="gender" class="gen">
