b83ef26c94f1aea9e3ecfa5b9f9fab2435eb8aaa
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 02:53:44 +0400

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,13 @@
   }
 ?>
     </form>
-
+    
+    <div>
+      <input type="text" name="filter" class="btn" placeholder="Введите значение">
+      <br><br>
+      <input type="submit" name="filterBtn" class="btn" value="Фильтр">
+    </div>
+    
     <?php
     try {
       $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
@@ -89,7 +95,34 @@
       echo "<h3>No one is currently registered.</h3>";
     }
     
-     
+     if(isset($_POST['filterBtn']))
+{
+    $filter = $_POST['filter'];
+    $sql_select = "SELECT * FROM registration_tb LIKE '%".$filter."%'";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll();
+    if(count($registrants) > 0) {
+      echo "<h2>People who are registered:</h2>";
+      echo "<table>";
+      echo "<tr><th>Name</th>";
+      echo "<th>Email</th>";
+      echo "<th>Gender</th>";
+      echo "<th>Date</th></tr>";
+      foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['gender']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+      echo "<h3>No one is currently registered.</h3>";
+    }
+    
+}
+ else {
+     $sql_select = "SELECT * FROM registration_tb";
+}
     
     ?>
 
