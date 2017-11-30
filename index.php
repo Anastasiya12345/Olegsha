f99dd5582ee5f8c9a103bd353f13217612a8869d
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 13:37:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,31 @@
       </div>
      <?php
       
+       if(isset($_POST['filterBtn'])) {
+         $gens = $_POST['gens'];
+         $sql_select = "SELECT * FROM registration_tb LIKE '%".$gens."%'";
+         $stmt = $conn->query($sql_select);
+         $registrants = $stmt->fetchAll();
+         if(count($registrants) > 0) {
+           echo "<h2>People who are registered:</h2>";
+           echo "<table>";
+           echo "<tr><th>Name</th>";
+           echo "<th>Email</th>";
+           echo "<th>Gender</th>";
+           echo "<th>Date</th></tr>";
+           foreach($registrants as $registrant) {
+             echo "<tr><td>".$registrant['name']."</td>";
+             echo "<td>".$registrant['email']."</td>";
+             echo "<td>".$registrant['gender']."</td>";
+             echo "<td>".$registrant['date']."</td></tr>";
+           }
+           echo "</table>";
+         } 
+         else {
+           echo "<h3>No one is currently registered.</h3>";
+         }
+       }
+      
   try {
       $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
