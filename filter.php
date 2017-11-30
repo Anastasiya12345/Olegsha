df334ebf28b51e8024799688da62ccbdf71f54f8
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 13:50:01 +0300

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,9 +1,9 @@
  <?php
  
- if(isset($_POST['filterBtn']))
+ if(isset($_POST['filter']))
 {
-    $gens = $_POST['gens'];
-    $sql_select = "SELECT * FROM registration_tb LIKE '%".$gens."%'";
+    $gender = $_POST['gender'];
+    $sql_select = "SELECT * FROM registration_tb LIKE '%".$gender."%'";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
@@ -28,24 +28,6 @@
  else {
      $sql_select = "SELECT * FROM registration_tb";
      $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll();
-    if(count($registrants) > 0) {
-      echo "<h2>People who are registered:</h2>";
-      echo "<table>";
-      echo "<tr><th>Name</th>";
-      echo "<th>Email</th>";
-      echo "<th>Gender</th>";
-      echo "<th>Date</th></tr>";
-      foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-        echo "<td>".$registrant['email']."</td>";
-        echo "<td>".$registrant['gender']."</td>";
-        echo "<td>".$registrant['date']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-      echo "<h3>No one is currently registered.</h3>";
-    }
 }
 
 ?>
