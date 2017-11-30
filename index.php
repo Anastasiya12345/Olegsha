8786914bd22f5c99bee1829357dcec5ef24300c0
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 23:28:11 +0400

    Update index.php

--- index.php
+++ index.php
@@ -89,8 +89,26 @@
           echo "<td>".$registrant['gender']."</td>";
           echo "<td>".$registrant['date']."</td></tr>";
         }
-
-      
+        
+          if else (isset($_POST['filter']) {
+          $sql_select = "SELECT * FROM registration_tb LIKE '%".$gender."%'";
+          $stmt = $conn->query($sql_select);
+          $registrants = $stmt->fetchAll();
+          if(count($registrants) > 0) {
+            echo "<h2>Люди, которые зарегистрированы:</h2>";
+            echo "<table>";
+            echo "<tr><th>Name</th>";
+            echo "<th>Email</th>";
+            echo "<th>Gender</th>";
+            echo "<th>Date</th></tr>";
+            foreach($registrants as $registrant) {
+              echo "<tr><td>".$registrant['name']."</td>";
+              echo "<td>".$registrant['email']."</td>";
+              echo "<td>".$registrant['gender']."</td>";
+              echo "<td>".$registrant['date']."</td></tr>";
+            }
+        }
+        
         echo "</table>";
       }
       else {
