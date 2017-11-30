28ce0f62175b11d499dd1ad68b3d787dd5b04ffa
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 21:42:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
     <title>Регистрационная форма</title>
     <link rel ="stylesheet" href ="/style.css">
     <link rel ="stylesheet" href ="/form.css">
