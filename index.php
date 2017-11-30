06e75f0f59fe67d2cc60fd9bee18f224b00d7b09
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-11-30 17:41:32 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,86 +1,92 @@
 <html>
   <head>
-    <title>Registration Form</title>
-    <link rel="stylesheet" href="/style.css">
-    <link rel="stylesheet" href="/form.css">
+    <title>Регистрационная форма</title>
+    <link rel ="stylesheet" href ="/style.css">
+    <link rel ="stylesheet" href ="/form.css">
   </head>
   <body>
-    <h1>Register here!</h1>
-    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
-    
-    <form method="post" action="index.php" enctype="multipart/form-data">
+    <h1>Зарегистрироваться здесь!</h1>
+    <p>Введите свое имя и адрес электронной почты и нажмите кнопку <strong>Отправить</strong> для регистрации.</p>
+    <form method ="post" action ="index.php" enctype ="multipart/form-data">
       <div>
-        <input type="text" name="name" id="name" placeholder="Введите ваше имя">
-        <input type="text" name="email" id="email" placeholder="Ваш еmail..">
-        <input type="submit" name="submit" class="btn" value="Отправить">
+        <input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
+        <input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
+        <input type ="submit" name ="submit" class ="btn" value ="Отправить">
         <br>
-        <input type="submit" name="clear" class="btn" value="Очистить">
+        <input type ="submit" name ="clear" class ="btn" value ="Очистить">
       </div>
       <div>
-        <select name="gender" id="gender" class="gen">
+        <select name ="gender" id ="gender" class ="gen">
           <option>Выбирите ваш пол</option>
-          <option value="Man">Man</option>
-          <option value="Woman">Woman</option>
+          <option value ="Man">Man</option>
+          <option value ="Woman">Woman</option>
         </select>
-        <input type="submit" name="filter" class="btn" value="Фильтр">
+        <input type ="submit" name ="filter" class ="btn" value="Фильтр">
       </div>
-     <?php
       
-  try {
-      $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      if(isset($_POST["clear"])) {
-      $sql1 = "DELETE FROM registration_tb";
-      $conn->query($sql1);
+      <?php
+      try {
+        $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if(isset($_POST["clear"])) {
+          $sql1 = "DELETE FROM registration_tb";
+          $conn->query($sql1);
+        }
       }
-  }
-  catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-  }
-     
-?>
-      
-      
+      catch (PDOException $e) {
+        print("Ошибка подключения к SQL Server.");
+        die(print_r($e));
+      }
+      ?>
     </form>
-    
 
     <?php
     try {
-      include_once "filter.php";
       $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
-      print("Error connecting to SQL Server.");
+      print("Ошибка подключения к SQL Server.");
       die(print_r($e));
     }
     if(!empty($_POST)) {
-    try {
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $date = date("Y-m-d");
-      $gender = $_POST['gender'];
+      try {
+        $name   = $_POST['name'];
+        $email  = $_POST['email'];
+        $date   = date("Y-m-d");
+
+        if(isset($_POST['filter'])) {
+          $gender = $_POST['gender'];
+          $sql_insert ="INSERT INTO registration_tb (name, email, date, gender) VALUES (?,?,?,?)";
+          $stmt = $conn->prepare($sql_insert);
+          $stmt->bindValue(1, $name);
+          $stmt->bindValue(2, $email);
+          $stmt->bindValue(3, $date);
+          $stmt->bindValue(4, $gender);
+          $stmt->execute();
+        }
+        else {
+          $gender = $_POST['gender'];
+        }
         // Insert data
-      $sql_insert ="INSERT INTO registration_tb (name, email, date, gender) VALUES (?,?,?,?)";
-      $stmt = $conn->prepare($sql_insert);
-      $stmt->bindValue(1, $name);
-      $stmt->bindValue(2, $email);
-      $stmt->bindValue(3, $date);
-      $stmt->bindValue(4, $gender);
-      $stmt->execute();
+        $sql_insert ="INSERT INTO registration_tb (name, email, date, gender) VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+        $stmt->bindValue(4, $gender);
+        $stmt->execute();
+      }
+      catch(Exception $e) {
+        die(var_dump($e));
+      }
+      echo "<h3>Вы зарегистрировались!</h3>";
     }
-    catch(Exception $e) {
-      die(var_dump($e));
-    }
-    echo "<h3>Your're registered!</h3>";
-    }
-    $sql_select = "SELECT * FROM registration_tb";
-    $stmt = $conn->query($sql_select);
+    $sql_select  = "SELECT * FROM registration_tb";
+    $stmt        = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
-      echo "<h2>People who are registered:</h2>";
+      echo "<h2>Люди, которые зарегистрированы:</h2>";
       echo "<table>";
       echo "<tr><th>Name</th>";
       echo "<th>Email</th>";
@@ -91,17 +97,14 @@
         echo "<td>".$registrant['email']."</td>";
         echo "<td>".$registrant['gender']."</td>";
         echo "<td>".$registrant['date']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<h3>No one is currently registered.</h3>";
+      }
+      echo "</table>";
+    } 
+    else {
+      echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
     }
     
-    
-     
     ?>
     
-    
-
   </body>
 </html>
