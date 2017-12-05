e727ec72e2661649016a271187468f2e2695e4e6
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-12-06 01:08:17 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -2,7 +2,8 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:olezhka.database.windows.net,1433; Database = Prime", "Skaylans", "Lgj231997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tb(
+    if($sql == "") {
+        $sql = "CREATE TABLE registration_tb(
     id INT NOT NULL IDENTITY(1,1), 
     PRIMARY KEY(id),
     name VARCHAR(30),
@@ -10,10 +11,15 @@ try {
     gender VARCHAR(10),
     date DATE)";
     $conn->query($sql);
+    }
+    else {
+      echo "<h3>Таблица уже создана.</h3>"
+    }
+    
 }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
+    print("Ошибка подключения к SQL Server.");
     die(print_r($e));
 }
-echo "<h3>Table created.</h3>";
+echo "<h3>Таблица создана.</h3>";
 ?>
