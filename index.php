ea52c3baab93e0e17b492133c4796c688425ea1e
Skaylans <32162864+Skaylans@users.noreply.github.com>
2017-12-19 08:34:29 +0400

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
             <div>
              <input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
              <input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-             <input type ="text" name ="date" id ="date" placeholder ="Дата">
+             <input type ="date" name ="date" id ="date" placeholder ="Дата">
              <div>
               <input type ="submit" name ="submit"  class ="btn" value ="Отправить">     
               <input type ="submit" name ="clear" class ="btn" id = "clr" value ="Очистить"></pre>
